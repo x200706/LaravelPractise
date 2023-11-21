8c834d4522d826211c2d3d8363821c93b6586fd0
x200706 <99391710+x200706@users.noreply.github.com>
2023-11-21 06:51:20 +0000

    Routes Test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hihi', function () {
+    return 'hihiGet';
+});
+Route::get('param/{name}', function ($name) {
+    return 'hihiParam'.$name;
+});
+Route::post('hihi', function () {
+    return 'hihiPost';
+});
+Route::any('any', function () {
+    return 'hihiAny';
+});
+Route::match(['get', 'post'],'match', function () {
+    return 'hihiMatch';
+});
+
+Route::get('testctrl/{id}', [TestController::class, 'read']); // 得use
+
+// Route::get('testctrl２/{id}', 'App\Http\Controllers\TestController@read'); // 免use
+// 一個方法只能註冊一個路由？！
