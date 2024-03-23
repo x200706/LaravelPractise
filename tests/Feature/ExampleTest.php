116dc205bcdcd78fb0216965ec4a6489dc3dbba5
Crystal <99391710+x200706@users.noreply.github.com>
2024-03-23 13:42:32 +0000

    Add a new test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,27 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Carbon\Carbon;
 
-class ExampleTest extends TestCase
+class SumArrayTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @group Feature/SumArrayTest
      */
-    public function test_the_application_returns_a_successful_response()
+    public function testSumArray()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+      // select date_format(日期A, "%Y-%m-%d") as 日期A_format, sum(項目A) as 項目A總和, sum(項目B) as 項目B總和, 類型 from 表A
+      // where 流水號 = ? 
+      // and 日期A >= ? 
+      // and 日期B <= ? 
+      // and not (日期A =  
+      // (select MAX(日期A) from 表A where 流水號 = ?  and date_format(日期A, "%Y-%m-%d") = 2024-03-23) 
+      // and 項目A + 項目B < 1000)
+      // group by date_format(日期A, "%Y-%m-%d"), 類型
+      $mock_DB_result = [
+        [],
+        []
+      ];
     }
 }
