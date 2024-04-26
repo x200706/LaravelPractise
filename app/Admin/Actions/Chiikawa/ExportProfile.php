be3c6f635620b5b0d27efebcac171b8763c1737b
Crystal <99391710+x200706@users.noreply.github.com>
2024-04-26 17:53:04 +0000

    增加註釋

--- app/Admin/Actions/Chiikawa/ExportProfile.php
+++ app/Admin/Actions/Chiikawa/ExportProfile.php
@@ -3,7 +3,7 @@
 namespace App\Admin\Actions\Chiikawa;
 use Encore\Admin\Grid\Exporters\ExcelExporter; 
 
-class ExportProfile extends ExcelExporter
+class ExportProfile extends ExcelExporter // 繼承自動欄寬還可以美化Excel喔
 {
   protected $fileName = '匯出小可愛個人資料.xlsx';
 
@@ -13,4 +13,5 @@ class ExportProfile extends ExcelExporter
       'sign' => '星座',
       'birthday' => '生日',
   ];  
+  // 如果要輸出join表格，return值要模仿Controller提到的原生ORM關聯取值方法
 }
